3738e06cbb4ce6383c3b93261abe9bb26dc50730
klebster2 <42589399+klebster2@users.noreply.github.com>
2024-06-16 22:09:38 +0100

    updated tmux.conf

--- bash_functions/git.sh
+++ bash_functions/git.sh
@@ -5,9 +5,9 @@ git_config_change_user_credentials() {
 	# g - git, u - user, c - credentials
 	printf "Change name and email for current commit?\n"
 	for _option in user.name user.email; do
-		printf "git config --get ${_option} => "
+		printf "git config --get %s => " "${_option}"
 		git config --get "${_option}"
-		read -p "Change ${_option} (y/n/q)? " y_n_q
+		read -rp "Change ${_option} (y/n/q)? " y_n_q
 		msg="option selected"
 		case "$y_n_q" in
 			y|Y|Yes|yes ) echo "'${y_n_q}' $msg'"; git_change_user_info "${_option}";;
@@ -17,11 +17,20 @@ git_config_change_user_credentials() {
 		esac
 	done
 }
+git_switch_to_ssh_remote() {
+	remote_push="$(git remote -v | cut -d $'\t' -f2 | grep push | cut -d ' ' -f1)"
+	if echo "$remote_push" | grep -q ".git"; then
+		echo "Error - it seems there is already a .git in the current remote URL:"
+		git remote  -v
+	else
+		git remote set-url origin "git@github.com:$(echo "${remote_push//\/\/}" | cut -d '/' -f2,3).git"
+	fi
+}
 # >>> Functions without args. >>>
 # <<< Functions with args. <<<
 git_change_user_info() {
 	local _option="$1"
-	read -p "new $_option: " value
+	read -rp "new $_option: " value
 	git config --local "$_option" "$value" && echo "successfuly changed" \
 		|| echo "unsuccessful"
 }
